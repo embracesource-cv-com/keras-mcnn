fbcb12dfb5580db93fbeb42801e2476322b4b361
ybcc <271380178@qq.com>
2019-04-09 10:27:06 +0800

    modify the visualization of the heatmap

--- utils/heatmap.py
+++ utils/heatmap.py
@@ -2,6 +2,8 @@
 import numpy as np
 import os
 from pyheatmap.heatmap import HeatMap
+from PIL import Image
+import cv2
 
 
 def save_heatmap(density_map, blob, imgs_dir, output_dir, down_sample=True):
@@ -16,6 +18,7 @@ def save_heatmap(density_map, blob, imgs_dir, output_dir, down_sample=True):
     """
     img = blob['data']  # 图片数组, shape(h, w, 1)
     img_name = blob['fname']  # 图片文件名
+    counts = int(np.sum(density_map))  # 人数
     print('generating heatmap for', img_name)
 
     # 如果密度图进行下采样4倍, 则需要还原到原始大小
@@ -34,6 +37,18 @@ def save_heatmap(density_map, blob, imgs_dir, output_dir, down_sample=True):
             for k in range(int(density_map[row][col])):
                 data.append([col + 1, row + 1])
     # 生成heatmap
-    hm = HeatMap(data, base=os.path.join(imgs_dir, img_name))
-    # 保存
-    hm.heatmap(save_as=os.path.join(output_dir, 'heatmap_'+img_name.split('.')[0]+'.png'))
+    hm = HeatMap(data, width=w, height=h)
+    # 保存heatmap
+    hm_name = 'heatmap_'+img_name.split('.')[0]+'.png'
+    hm.heatmap(save_as=os.path.join(output_dir, hm_name))
+
+    # 使用蓝色填充heatmap背景, 并显示人群数量
+    im = Image.open(os.path.join(output_dir, hm_name))
+    x, y = im.size
+    bg = Image.new('RGBA', im.size, (0, 0, 139))
+    bg.paste(im, (0, 0, x, y), im)
+    im_arr = np.array(bg)
+    text = 'GT Count: {}'.format(counts)
+    cv2.putText(im_arr, text, (10, y - 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+    im = Image.fromarray(im_arr)
+    im.save(os.path.join(output_dir, hm_name))
