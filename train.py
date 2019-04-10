8140f045013c442072a6d1e6f7f302ec8790a12c
ybcc <271380178@qq.com>
2019-04-10 11:24:48 +0800

    use generator to train

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 _*-
+# -*- coding:utf-8 -*-
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from model import MCNN
@@ -17,11 +17,9 @@ def main(args):
     train_gt_path = cfg.TRAIN_GT_PATH.format(dataset)
     val_path = cfg.VAL_PATH.format(dataset)
     val_gt_path = cfg.VAL_GT_PATH.format(dataset)
-    # 生成训练数据和验证数据
+    # 加载数据生成器
     train_data_gen = DataLoader(train_path, train_gt_path, shuffle=True, gt_downsample=True)
     val_data_gen = DataLoader(val_path, val_gt_path, shuffle=False, gt_downsample=True)
-    x_train, y_train = train_data_gen.get_all()
-    x_val, y_val = val_data_gen.get_all()
 
     # 定义模型
     input_shape = (None, None, 1)
@@ -31,26 +29,20 @@ def main(args):
 
     # 定义callback
     checkpointer_best_train = ModelCheckpoint(
-        filepath=os.path.join(cfg.MODEL_DIR, 'mcnn_'+dataset+'_train_generator.hdf5'),
+        filepath=os.path.join(cfg.MODEL_DIR, 'mcnn_'+dataset+'_train.hdf5'),
         monitor='loss', verbose=1, save_best_only=True, mode='min'
     )
     callback_list = [checkpointer_best_train]
 
     # 训练
     print('Training Part_{} ...'.format(dataset))
-    model.fit(
-        x=x_train, y=y_train, batch_size=1, epochs=cfg.EPOCHS,
-        validation_data=(x_val, y_val),
-        callbacks=callback_list
-    )
-
-    # model.fit_generator(train_data_gen.flow(cfg.TRAIN_BATCH_SIZE),
-    #                     steps_per_epoch=train_data_gen.num_samples // cfg.TRAIN_BATCH_SIZE,
-    #                     validation_data=val_data_gen.flow(cfg.VAL_BATCH_SIZE),
-    #                     validation_steps=val_data_gen.num_samples // cfg.VAL_BATCH_SIZE,
-    #                     epochs=cfg.EPOCHS,
-    #                     callbacks=callback_list,
-    #                     verbose=1)
+    model.fit_generator(train_data_gen.flow(cfg.TRAIN_BATCH_SIZE),
+                        steps_per_epoch=train_data_gen.num_samples // cfg.TRAIN_BATCH_SIZE,
+                        validation_data=val_data_gen.flow(cfg.VAL_BATCH_SIZE),
+                        validation_steps=val_data_gen.num_samples // cfg.VAL_BATCH_SIZE,
+                        epochs=cfg.EPOCHS,
+                        callbacks=callback_list,
+                        verbose=1)
 
 
 if __name__ == '__main__':
