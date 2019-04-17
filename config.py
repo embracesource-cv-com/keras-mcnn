e7180a5ac53028d0521085bd82ea235d6c95bcdd
ybcc <271380178@qq.com>
2019-04-17 09:46:55 +0800

    define a Config class

--- config.py
+++ config.py
@@ -1,26 +1,31 @@
-# This file is used to save the global configurations
+# -*- coding:utf-8 -*-
 
-# 模型存放目录
-MODEL_DIR = './trained_models/'
 
-# 训练集目录
-TRAIN_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train'
-# 训练集Ground-Truth目录
-TRAIN_GT_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train_den'
+class Config(object):
+    # 模型存放目录
+    MODEL_DIR = './trained_models/'
 
-# 验证集目录
-VAL_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val'
-# 验证集Ground_Truth目录
-VAL_GT_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val_den'
+    # 训练集目录
+    TRAIN_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train'
+    # 训练集Ground-Truth目录
+    TRAIN_GT_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/train_den'
 
-# 测试集目录
-TEST_PATH = './data/original/shanghaitech/part_{}_final/test_data/images/'
-# 测试集Ground_Truth目录
-TEST_GT_PATH = './data/original/shanghaitech/part_{}_final/test_data/ground_truth_csv/'
+    # 验证集目录
+    VAL_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val'
+    # 验证集Ground_Truth目录
+    VAL_GT_PATH = './data/formatted_trainval_{0}/shanghaitech_part_{0}_patches_9/val_den'
 
-# 测试集Ground_Truth heatmap目录
-HM_GT_PATH = './heatmaps_gt'
+    # 测试集目录
+    TEST_PATH = './data/original/shanghaitech/part_{}_final/test_data/images/'
+    # 测试集Ground_Truth目录
+    TEST_GT_PATH = './data/original/shanghaitech/part_{}_final/test_data/ground_truth_csv/'
 
-EPOCHS = 200
-TRAIN_BATCH_SIZE = 1
-VAL_BATCH_SIZE = 1
+    # 测试集Ground_Truth heatmap目录
+    HM_GT_PATH = './heatmaps_gt'
+
+    EPOCHS = 200
+    TRAIN_BATCH_SIZE = 1
+    VAL_BATCH_SIZE = 1
+
+
+current_config = Config()
