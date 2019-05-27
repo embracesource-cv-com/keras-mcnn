9cbaeb40e3e79472404aa13df68415f1648fb0db
yizt <csuyzt@163.com>
2019-05-27 18:00:35 +0800

    重构测试集gt生成过程

--- create_gt_test_set_shtech.py
+++ create_gt_test_set_shtech.py
@@ -4,14 +4,17 @@ import cv2
 import csv
 import argparse
 from scipy.io import loadmat
-import gen_density_map
+from data_preparation.density_map import gen_density_map
+from config import current_config as cfg
 
 
 def main(args):
     dataset = args.dataset
-    path = ''.join(['./data/original/part_', dataset, '_final/test_data/images/'])
-    gt_path = ''.join(['./data/original/part_', dataset, '_final/test_data/ground_truth/'])
-    gt_path_csv = ''.join(['./data/original/part_', dataset, '_final/test_data/ground_truth_csv/'])
+    cfg.init_path(dataset)  # 初始化路径名
+    image_path = os.path.join(cfg.ORIGIN_DATA_PATH, 'test_data/images')
+    gt_path = os.path.join(cfg.ORIGIN_DATA_PATH, 'test_data/ground_truth')
+    gt_path_csv = os.path.join(cfg.ORIGIN_DATA_PATH, 'test_data/ground_truth_csv')
+
     if not os.path.exists(gt_path_csv):
         os.makedirs(gt_path_csv)
     if dataset == 'A':
@@ -19,16 +22,17 @@ def main(args):
     else:
         num_images = 316
 
-    for i in range(1, num_images+1):
+    for i in range(1, num_images + 1):
         if i % 10 == 0:
-            print('Processing {}/{} files'.format(i, num_images), '\nwriting to {}'.format(''.join([gt_path_csv, 'IMG_', str(i), '.csv'])))
-        image_info = loadmat(''.join((gt_path, 'GT_IMG_', str(i), '.mat')))['image_info']
-        input_img_name = ''.join((path, 'IMG_', str(i), '.jpg'))
-        im = cv2.imread(input_img_name, 0)
-        annPoints = image_info[0][0][0][0][0] - 1
-        im_density = gen_density_map.gen_density_map(im, annPoints)
-        with open(''.join([gt_path_csv, 'IMG_', str(i), '.csv']), 'w', newline='') as fout:
-            writer = csv.writer(fout)
+            print('Processing {}/{} files'.format(i, num_images),
+                  '\nwriting to {}'.format(''.join([gt_path_csv, 'IMG_', str(i), '.csv'])))
+        image_info = loadmat(os.path.join(gt_path, 'GT_IMG_{}.mat'.format(i)))['image_info']
+        input_img_path = os.path.join(image_path, 'IMG_{}.jpg'.format(i))
+        im = cv2.imread(input_img_path, 0)
+        points = image_info[0][0][0][0][0] - 1
+        im_density = gen_density_map(im, points)
+        with open(os.path.join(gt_path_csv, 'IMG_{}.csv'.format(i)), 'w', newline='') as f:
+            writer = csv.writer(f)
             writer.writerows(im_density)
 
 
