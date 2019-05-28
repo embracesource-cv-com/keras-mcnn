5db6cc943b4c6e485d15d025b933a289e317e206
yizt <csuyzt@163.com>
2019-05-28 10:40:53 +0800

    重构训练过程

--- train.py
+++ train.py
@@ -18,14 +18,19 @@ import argparse
 def main(args):
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     dataset = args.dataset  # 'A' or 'B'
+    cfg.init_path(dataset)  # 初始化路径名
 
-    train_path = cfg.TRAIN_PATH.format(dataset)
-    train_gt_path = cfg.TRAIN_GT_PATH.format(dataset)
-    val_path = cfg.VAL_PATH.format(dataset)
-    val_gt_path = cfg.VAL_GT_PATH.format(dataset)
     # 加载数据生成器
-    train_data_gen = DataLoader(train_path, train_gt_path, shuffle=True, gt_downsample=True)
-    val_data_gen = DataLoader(val_path, val_gt_path, shuffle=False, gt_downsample=True)
+    train_data_gen = DataLoader(cfg.TRAIN_PATH,
+                                cfg.TEST_GT_PATH,
+                                batch_size=cfg.TRAIN_BATCH_SIZE,
+                                shuffle=True,
+                                gt_downsample=True)
+    val_data_gen = DataLoader(cfg.VAL_PATH,
+                              cfg.VAL_GT_PATH,
+                              batch_size=cfg.VAL_BATCH_SIZE,
+                              shuffle=False,
+                              gt_downsample=True)
 
     # 定义模型
     input_shape = (None, None, 1)
@@ -35,17 +40,19 @@ def main(args):
 
     # 定义callback
     checkpointer_best_train = ModelCheckpoint(
-        filepath=os.path.join(cfg.MODEL_DIR, 'mcnn_'+dataset+'_train.hdf5'),
-        monitor='loss', verbose=1, save_best_only=True, mode='min'
+        filepath=cfg.WEIGHT_PATH,
+        monitor='val_loss',
+        verbose=1,
+        save_best_only=True,
+        save_weights_only=True,
+        mode='min'
     )
     callback_list = [checkpointer_best_train]
 
     # 训练
     print('Training Part_{} ...'.format(dataset))
-    model.fit_generator(train_data_gen.flow(cfg.TRAIN_BATCH_SIZE),
-                        steps_per_epoch=train_data_gen.num_samples // cfg.TRAIN_BATCH_SIZE,
-                        validation_data=val_data_gen.flow(cfg.VAL_BATCH_SIZE),
-                        validation_steps=val_data_gen.num_samples // cfg.VAL_BATCH_SIZE,
+    model.fit_generator(train_data_gen,
+                        validation_data=val_data_gen,
                         epochs=cfg.EPOCHS,
                         callbacks=callback_list,
                         verbose=1)
