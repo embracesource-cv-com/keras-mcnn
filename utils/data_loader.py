5d980117334b62d4d936ec15ac86327da5738729
yizt <csuyzt@163.com>
2019-05-28 10:40:41 +0800

    重构数据加载

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -9,68 +9,74 @@ import numpy as np
 import cv2
 import os
 import pandas as pd
+from keras.utils import Sequence
 
 
-class DataLoader(object):
-    def __init__(self, data_path, gt_path, shuffle=False, gt_downsample=False):
+class DataLoader(Sequence):
+    def __init__(self, data_path, gt_path, batch_size=1, shuffle=False, gt_downsample=False):
         """
         :param data_path: 图片文件路径
         :param gt_path: ground truth路径
+        :param batch_size:
         :param shuffle: bool, 是否打乱数据
         :param gt_downsample: bool, 是否下采样
         """
         self.data_path = data_path
         self.gt_path = gt_path
+        self.batch_size = batch_size
         self.shuffle = shuffle
         self.gt_downsample = gt_downsample
-        self.data_files = [filename for filename in os.listdir(data_path)]
-        self.num_samples = len(self.data_files)
-        self.blob_list = []
+        self.filename_list = [filename for filename in os.listdir(data_path)]
 
-        print('Loading data, wait a moment...')
-        for fname in self.data_files:
-            img = cv2.imread(os.path.join(self.data_path, fname), 0)
-            img = img.astype(np.float32, copy=False)
-            ht = img.shape[0]
-            wd = img.shape[1]
-            ht_1 = int((ht / 4) * 4)
-            wd_1 = int((wd / 4) * 4)
-            img = cv2.resize(img, (wd_1, ht_1))
-            img = img.reshape((img.shape[0], img.shape[1], 1))
-            den = pd.read_csv(os.path.join(self.gt_path, os.path.splitext(fname)[0] + '.csv'),
-                              header=None).values
-            den = den.astype(np.float32, copy=False)
-            if self.gt_downsample:
-                wd_1 = int(wd_1 / 4)
-                ht_1 = int(ht_1 / 4)
-            den = cv2.resize(den, (wd_1, ht_1))
-            den = den * ((wd * ht) / (wd_1 * ht_1))
-            den = den.reshape((den.shape[0], den.shape[1], 1))
+    def __getitem__(self, item):
+        """
 
-            blob = dict()
-            blob['data'] = img
-            blob['gt'] = den
-            blob['fname'] = fname
-            self.blob_list.append(blob)
+        :param item: 索引编号 int
+        :return:
+        """
+        ix = np.arange(self.batch_size * item, self.batch_size * (item + 1))
+        images, dens = [], []
+        for index in ix:
+            im, den = self._load(self.filename_list[index])
+            images.append(im)
+            dens.append(den)
 
+        # 转为numpy返回
+        return np.array(images), np.array(dens)
+
+    def __len__(self):
+        return len(self.filename_list) // self.batch_size
+
+    def on_epoch_end(self):
         if self.shuffle:
-            np.random.shuffle(self.blob_list)
+            np.random.shuffle(self.filename_list)
 
-    def flow(self, batch_size=32):
-        loop_count = self.num_samples // batch_size
-        while True:
-            np.random.shuffle(self.blob_list)
-            for i in range(loop_count):
-                blobs = self.blob_list[i*batch_size: (i+1)*batch_size]
-                X_batch = np.array([blob['data'] for blob in blobs])
-                Y_batch = np.array([blob['gt'] for blob in blobs])
-                yield X_batch, Y_batch
+    def _load(self, filename):
+        """
 
-    def get_all(self):
-        X = np.array([blob['data'] for blob in self.blob_list])
-        Y = np.array([blob['gt'] for blob in self.blob_list])
-        return X, Y
+        :param filename: 图像文件名  "abc.jpg"
+        :return: image: numpy数组[H,W,1]
+        :return: density_map: numpy数组 [h,w,1]
+        """
+        image_path = os.path.join(self.data_path, filename)
+        density_name = os.path.splitext(filename)[0] + '.csv'
+        density_path = os.path.join(self.gt_path, density_name)
+        # 加载图像
+        im = cv2.imread(image_path, 0)
+        im = im.astype(np.float32, copy=False)
+        # 保证长宽可以被4整除，网络的步长为4
+        h, w = im.shape
+        im = cv2.resize(im, (w // 4 * 4, h // 4 * 4))
 
-    def __iter__(self):
-        for blob in self.blob_list:
-            yield blob
+        # 加载密度图
+        den = pd.read_csv(density_path, header=None).values
+        den = den.astype(np.float32, copy=False)
+        if self.gt_downsample:
+            den = cv2.resize(den, (w // 4, h // 4))
+        scale_factor = w * h / ((w // 4) * (h // 4))
+        den *= scale_factor
+
+        # 扩展通道维返回
+        return im[:, :, np.newaxis], den[:, :, np.newaxis]
+
+
